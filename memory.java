2c38021a90935f4b823c17613241213b7ce20fcc
Hitesh Salunkhe <85603738+salunkhe12@users.noreply.github.com>
2023-11-09 10:52:22 +0530

    Add files via upload

---
+++ memory.java
@@ -0,0 +1,13 @@
+class memory
+{
+ public static void main(String args[])
+ {
+  System.out.println("Byte = " + Byte.SIZE/8);
+  System.out.println("Short = " + Short.SIZE/8);
+  System.out.println("Integer = " + Integer.SIZE/8);
+  System.out.println("Long = " + Long.SIZE/8);
+  System.out.println("Float = " + Float.SIZE/8);
+  System.out.println("Double = "  + Double.SIZE/8);
+  System.out.println("Character = " + Character.SIZE/8);
+ }
+}
